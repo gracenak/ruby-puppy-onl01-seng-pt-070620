2dee0b8d47ba318ba91a29ce0f124206b954574f
Yuiko Grace Nakano <gracenak@gmail.com>
2020-07-24 02:02:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,42 @@
-# Add your code here
+#require 'pry'
+class Dog
+  
+  @@all = []
+  
+  attr_accessor :name, :save
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+    
+  def self.all
+    @@all
+  end
+  
+  def self.clear_all
+    @@all.clear
+  end
+
+  def self.print_all
+    @@all.each do |name|
+      if print_all[name] 
+        print_all[name] += 1
+      else
+        print_all[name]
+   # binding.pry
+    end
+  end
+end
+
+
+    
+    
+    
+    
+    
+    
+    
+end    
+    
+    # Add your code here
